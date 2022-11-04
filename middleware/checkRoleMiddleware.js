bd8e5298a51c67ca4744fa707621736a275f1f33
Sergey <sergey.potapov.2002@mail.ru>
2022-11-04 15:47:05 +0500

    refer to the update frontend at 04.11.2022

--- middleware/checkRoleMiddleware.js
+++ middleware/checkRoleMiddleware.js
@@ -5,15 +5,19 @@ class checkRoleMiddleware {
         this.req = req;
         this.res = res;
         this.next = next;
-    };
+    }
 
     static admin(req, res, next) {
-        if (req.user.role === 'ADMIN') {
+        if (req.user.role === "ADMIN") {
             next();
         } else {
-            next(ApiError.forbidden(`The action is avaliable only for users with 'ADMIN' role.`))
-        };
-    };
-};
+            next(
+                ApiError.forbidden(
+                    `The action is avaliable only for users with 'ADMIN' role.`
+                )
+            );
+        }
+    }
+}
 
 module.exports = checkRoleMiddleware;
--- routes/characterRouter.js
+++ routes/characterRouter.js
@@ -1,14 +1,14 @@
-const Router = require('express');
+const Router = require("express");
 const router = new Router();
-const characterController = require('../controllers/characterController');
-const authMiddleware = require('../middleware/authMiddleware');
-const checkRoleMiddleware = require('../middleware/checkRoleMiddleware');
+const characterController = require("../controllers/characterController");
+const authMiddleware = require("../middleware/authMiddleware");
+const checkRoleMiddleware = require("../middleware/checkRoleMiddleware");
 
-router.get('/:URI', characterController.getOne);
-router.get('/', characterController.getAll);
-router.post('/', authMiddleware, characterController.create);
-router.put('/:id', authMiddleware, characterController.update);
-router.delete('/:id', authMiddleware, characterController.delete);
+router.get("/:URI", characterController.getOne);
+router.get("/", characterController.getAll);
+router.post("/", authMiddleware, characterController.create);
+router.put("/:id", authMiddleware, characterController.update);
+router.delete("/:id", authMiddleware, characterController.delete);
 
 module.exports = router;
 
